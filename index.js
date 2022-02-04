d38bbc2aa00ff1e6fa2ce3e173b821b17bd568ba
ÁNGEL DÍAZ <angel_diaz_murillo@hotmail.com>
2022-02-04 09:52:22 +0100

    ifc.js code update

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
 import {IfcAPI} from "web-ifc/web-ifc-api"
 
-const ifcApi=new IfcAPI ()
-ifcApi.Init()
+//Crea instancia de la API de IFC.js
+const ifcapi=new IfcAPI()
 
-console.log('hola')
-const button = document.getElementById('input-button')
-const input=document.getElementById('input-element')
+//Crea referencias de los objetos gráficos de la app
+const button = document.getElementById("file-opener-button");
+const leftContainer = document.getElementById("left-container");
+const saveButton = document.getElementById("save-button");
+const json = document.getElementById("json");
+const input = document.getElementById("file-input");
 
-button.onclick= () => {
-    input.click()
+//Cargado de archivos del fichero
+button.addEventListener('click', () => input.click())
+input.addEventListener(
+    "change",
+    (changed) =>{
+        const reader= new FileReader();
+        reader.onload= () => LoadFile(reader.result)
+        reader.readAsText(changed.target.files[0])
+    },
+    false
+)
+
+//Carga el ifc en la API
+function LoadFile(ifcAsText){
+    leftContainer.innerHTML = ifcAsText.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    const modelID=OpenIfc(ifcAsText)
+    const allItems=GetAllItems(modelID)
+    const result=JSON.stringify(allItems,undefined,2)
+    json.textContent=result
+    const blob=new Blob([result],{type:'aplication/json'})
+    saveButton.href=URL.createObjectURL(blob)
+    saveButton.download='data.json'
+    saveButton.click()
+    ifcapi.CloseModel(modelID)
 }
 
-input.onchange=(changed)=>{
-    console.log(changed)
-    const reader= new FileReader()
-    reader.onload= () => loadIfc(reader.result)
-    reader.readAsText(changed.target.files[0])
+function OpenIfc(ifcAsText){
+    ifcapi.Init()
+    return ifcapi.OpenModel(ifcAsText)
+
+}
+function GetAllItems(modelID,excludeGeometry=true){
+    const allItems={}
+    const lines= ifcapi.GetAllLines(modelID)    
+    GetAllItemsFromLines(modelID,lines,allItems,excludeGeometry)
+    return allItems
 }
 
-function loadIfc(IfcData) {
-    console.log(IfcData)
-    const modelID = ifcApi.OpenModel(IfcData)
-    const allItems=ifcApi.GetAllLines(modelID)
+function GetAllItemsFromLines(modelID,lines,allItems,excludeGeometry){
+    for( let i=1; i<= lines.size(); i++) {
+        try{
+            saveProperties(modelID,lines,allItems,excludeGeometry,i)
 
+        } catch (e){
+            console.log(e)
+        }
+    }
+}
+
+function saveProperties(modelID,lines,allItems,excludeGeometry,index){
+    const itemID=lines.get(index)
+    const props=ifcapi.GetLine(modelID,itemID)
+    props.type=props.__proto__.constructor.name
+    if (!excludeGeometry || !geometryTypes.has(props.type)){
+        allItems[itemID]=props
+    }
 }
